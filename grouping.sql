d01894a7014e8aeceb81437130ee1c7ad04fcb9c
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-04 18:18:20 +0600

    Update grouping.sql

--- grouping.sql
+++ grouping.sql
@@ -7,3 +7,11 @@ SELECT country, COUNT(*) FROM person GROUP BY countr ORDER BY count DESC;
 SELECT country, COUNT(*) FROM person GROUP BY country HAVING COUNT(*) > 5 ORDER BY country DESC;
 --Retrieves the number of people in each country from the person table, groups them by country, and orders them in descending order based on the country name, but only shows countries with more than 5 people using the HAVING clause.
 
+SELECT MAX(price) FROM car;
+--Returns the maximum value of the 'price' column in the 'car' table
+
+SELECT MIN(price) FROM car;
+--Returns the minimum value of the 'price' column in the 'car' table
+
+SELECT AVG(price) FROM car;
+--Returns the average value of the 'price' column in the 'car' table
