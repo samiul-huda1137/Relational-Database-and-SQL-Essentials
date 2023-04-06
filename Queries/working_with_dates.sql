ec133ba3cd44439a55651fbc6f0f30cdf6f566f8
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-06 21:17:36 +0600

    extraction from timestamps

--- Queries/working_with_dates.sql
+++ Queries/working_with_dates.sql
@@ -6,3 +6,15 @@ SELECT NOW()::DATE;
 
 SELECT NOW()::TIME;
 --will return only the current time in the format of 'HH:MI:SS'.
+
+SELECT EXTRACT (YEAR FROM NOW());
+--Extracts the current year from the timestamp returned by the NOW() function.
+
+SELECT EXTRACT (MONTH FROM NOW());
+--Extracts the current month from the timestamp returned by the NOW() function.
+
+SELECT EXTRACT (DAY FROM NOW());
+--Extracts the current day from the timestamp returned by the NOW() function.
+
+SELECT EXTRACT (DOW FROM NOW());
+--Extracts the day of the week as a number (0 for Sunday, 1 for Monday, and so on) from the timestamp returned by the NOW() function.
