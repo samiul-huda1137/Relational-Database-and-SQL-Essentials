f1906b6792af57bb88849d4ea78699296c66c35a
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-06 15:47:58 +0600

    use of COALESCE for null

---
+++ handling_null.sql
@@ -0,0 +1,3 @@
+SELECT COALESCE(email, 'Email not provided') FROM person;
+--This query uses the COALESCE function to replace NULL values in the "email" column with the string "Email not provided".
+
