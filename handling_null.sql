b556e26c8ef6ad698dd55f25a3ae7591666aaac8
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-06 15:55:28 +0600

    division by zero with NULLIF

--- handling_null.sql
+++ handling_null.sql
@@ -1,3 +1,5 @@
 SELECT COALESCE(email, 'Email not provided') FROM person;
 --This query uses the COALESCE function to replace NULL values in the "email" column with the string "Email not provided".
 
+SELECT COALESCE(10 / NULLIF(0,0),0);
+--return 0 instead of an error when trying to divide a number by 0, using NULLIF to handle this.
