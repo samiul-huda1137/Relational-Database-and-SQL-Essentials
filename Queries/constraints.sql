7187a997a994089fa7307eeb9cf5280c2161b220
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-07 23:05:58 +0600

    ALTER TABLE query

---
+++ Queries/constraints.sql
@@ -0,0 +1,4 @@
+ALTER TABLE person DROP CONSTRAINT person_pkey;
+--This SQL statement removes the primary key constraint named "person_pkey" from the "person" table.
+--This means that the table will no longer have a primary key constraint and you can add a new primary key constraint to the table or modify the existing one.
+
