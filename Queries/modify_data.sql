8e0fab4d6fc5bdaa7719bde2349eb454a9d80846
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-04-10 17:31:07 +0600

    updating records

--- Queries/modify_data.sql
+++ Queries/modify_data.sql
@@ -1,3 +1,11 @@
 DELETE FROM person WHERE age >= 100;
 --Delete all records from the "person" table where the age is greater than or equal to 100
 
+UPDATE person SET country='USA' WHERE id=1;
+--Update the "country" column of the "person" table for a specific record with id=1
+
+UPDATE car SET price = price * 1.1;
+--Update the "price" column of the "car" table by increasing it by 10% for all records
+
+DELETE FROM car WHERE id = 5;
+--Delete a specific record from the "car" table with id=5
